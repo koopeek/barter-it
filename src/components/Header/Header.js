690319c1ffba5fa4a7dcb424d3405789a9f01581
koopeek <slawekkopaczewski@gmail.com>
2020-03-16 21:46:03 +0100

    Add basic responsive header styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,37 @@ import { Link } from 'react-router-dom';
 import './Header.scss';
 
 const Header = () => {
+  const toggleHamburger = () => {
+    const hamburgerButton = document.querySelector('.navigation__hamburger');
+    const navigationList = document.querySelector('.navigation__list');
+    hamburgerButton.classList.toggle('navigation__hamburger--active');
+    navigationList.classList.toggle('navigation__list--active');
+  };
+
   return (
-    <header className="header">
-      <Link to="/item/new">Dodaj nowy produkt</Link>
+    <header>
+      <nav className="navigation">
+        <div className="navigation__logo">
+          <h4>Barter it</h4>
+        </div>
+        <ul className="navigation__list">
+          <li className="navigation__list__item">
+            <Link to="/" className="logo">
+              Barter it
+            </Link>
+          </li>
+          <li className="navigation__list__item">
+            <Link to="/item/new" className="logo">
+              Dodaj nowy przedmiot
+            </Link>
+          </li>
+        </ul>
+        <button className="navigation__hamburger" onClick={() => toggleHamburger()}>
+          <div className="navigation__hamburger__box">
+            <span className="navigation__hamburger__line"></span>
+          </div>
+        </button>
+      </nav>
     </header>
   );
 };
