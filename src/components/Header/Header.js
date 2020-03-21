90095832322316e508875c095d6a0a9d754df573
koopeek <slawekkopaczewski@gmail.com>
2020-03-21 19:47:48 +0100

    Fix navigation on mobile and tablet

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,18 +14,13 @@ const Header = () => {
     <header className="header">
       <nav className="navigation">
         <div className="navigation__logo">
-          <h4>Barter it</h4>
+          <h2>
+            <Link to="/">Barter it</Link>
+          </h2>
         </div>
         <ul className="navigation__list">
           <li className="navigation__list__item">
-            <Link to="/" className="logo">
-              Barter it
-            </Link>
-          </li>
-          <li className="navigation__list__item">
-            <Link to="/item/new" className="logo">
-              Dodaj nowy przedmiot
-            </Link>
+            <Link to="/item/new">Dodaj nowy przedmiot</Link>
           </li>
         </ul>
         <button className="navigation__hamburger" onClick={() => toggleHamburger()}>
