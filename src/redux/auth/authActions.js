5f52c6eaa3656c639e95be53bd33fbee0915acd0
koopeek <slawekkopaczewski@gmail.com>
2020-04-18 15:56:44 +0200

    Finish work on LoginForm component

--- src/redux/auth/authActions.js
+++ src/redux/auth/authActions.js
@@ -1,28 +1,41 @@
 import {
+  USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGIN_FAILURE,
   USER_LOGOUT_SUCCESS,
+  USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
-  USER_REGISTER_FAILURE
+  USER_REGISTER_FAILURE,
+  CLEAR_ERROR_MESSAGE
 } from './authActionTypes';
 
 export const loginUser = credentials => {
   return async dispatch => {
-    let headers = new Headers();
-    headers.append('Content-Type', 'application/json');
-    headers.append('Accept', 'application/json');
-
+    dispatch(loginRequest());
     await fetch(`https://barter-it-api.herokuapp.com/login`, {
       method: 'POST',
       body: JSON.stringify(credentials),
-      headers: headers
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+      }
     })
       .then(res => res.json())
       .then(res => {
-        localStorage.setItem('token', res.token);
-        dispatch(loginSuccess(res));
+        if (res.token) {
+          localStorage.setItem('token', res.token);
+          dispatch(loginSuccess(res));
+        } else {
+          dispatch(loginFailure('Nieprawidłowy adres e-mail lub hasło'));
+        }
       })
       .catch(err => dispatch(loginFailure(err)));
+  };
+};
+
+export const loginRequest = () => {
+  return {
+    type: USER_LOGIN_REQUEST
   };
 };
 
@@ -32,9 +45,10 @@ export const loginSuccess = () => {
   };
 };
 
-export const loginFailure = () => {
+export const loginFailure = errorMessage => {
   return {
-    type: USER_LOGIN_FAILURE
+    type: USER_LOGIN_FAILURE,
+    payload: errorMessage
   };
 };
 
@@ -44,14 +58,27 @@ export const logoutSuccess = () => {
   };
 };
 
+export const registerRequest = () => {
+  return {
+    type: USER_REGISTER_REQUEST
+  };
+};
+
 export const registerSuccess = () => {
   return {
     type: USER_REGISTER_SUCCESS
   };
 };
 
-export const registerFailure = () => {
+export const registerFailure = errorMessage => {
   return {
-    type: USER_REGISTER_FAILURE
+    type: USER_REGISTER_FAILURE,
+    payload: errorMessage
+  };
+};
+
+export const clearErrorMessage = () => {
+  return {
+    type: CLEAR_ERROR_MESSAGE
   };
 };
