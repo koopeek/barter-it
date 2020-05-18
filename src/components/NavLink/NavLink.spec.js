40a02dc2547c3687720e34f55f953d62b5218031
koopeek <slawekkopaczewski@gmail.com>
2020-05-18 22:04:49 +0200

    Configure react-testing-library and add basic test of NavLink

---
+++ src/components/NavLink/NavLink.spec.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router';
+import { render } from '@testing-library/react';
+import { NavLink } from './NavLink';
+
+test('contain proper text and path', () => {
+  const contentText = 'Some text';
+  const path = '/test';
+
+  const { getByText } = render(
+    <MemoryRouter initialEntries={['/']}>
+      <NavLink contentText={contentText} path={path} />
+    </MemoryRouter>
+  );
+
+  expect(getByText(contentText)).toBeInTheDocument();
+  expect(getByText(contentText).closest('a')).toHaveAttribute('href', path);
+});
